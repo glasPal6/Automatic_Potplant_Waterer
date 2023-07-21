18ce29d266225f9e421802fbd6fdfad54a3db756
glaspal6 <dylan.kamstra@protonmail.com>
2023-07-21 17:22:34 +0200

    Added push to the database

--- ESP_Program/src/main.cpp
+++ ESP_Program/src/main.cpp
@@ -2,133 +2,221 @@
 
 #include <Arduino.h>
 //------------------------------
-#include <ESP8266WiFi.h>
+#if defined(ESP8266)
+    #include <ESP8266WiFi.h>
+#elif defined(ESP32)
+    #include <WiFi.h>
+#endif
 
 #include <Firebase_ESP_Client.h>
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
+#include <time.h>
 #include <internet_details.h>
 //------------------------------
+struct sensor_data {
+    float temperature;
+    float humidity;
+    float soil_moisture;
+    float light_intensity;
+    float water_level;
+};
+const byte sensor_data_size = 5;
+byte hour = 0;
 
 FirebaseData firebaseData;
 FirebaseAuth firebaseAuth;
 FirebaseConfig firebaseConfig;
 
-unsigned long previousMillis = 0;
-const long interval = 1000;
-unsigned int count = 0;
+
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 2 * 3600;
+const int   daylightOffset_sec = 0;
 
 //------------------------------
 //Function declarations
 
 //Initialize functions
 void Wifi_Init();
+void NTP_Init();
 void Firebase_Init();
 void checkFirebaseConnection();
 
-void databaseTest();
+tm getDateAndTime();
+sensor_data getSensorData();
+bool writeToFirebase(String path, sensor_data data);
 
 //------------------------------
 // Main functions
 void setup() {
-  Serial.begin(115200);
-  Wifi_Init();
-  Firebase_Init();
-  delay(1000);
+    // Initialize the serial
+    Serial.begin(115200);
+    
+    // Initialize the components
+    Wifi_Init();
+    NTP_Init();
+    Firebase_Init();
+    
+    // Set the first hout
+    tm timeinfo = getDateAndTime();
+    hour = timeinfo.tm_hour;
+
+    delay(1000);
 }
 
 void loop() {
-  checkFirebaseConnection();
-  databaseTest();
+    checkFirebaseConnection();
+    
+    tm timeinfo = getDateAndTime();
+    sensor_data data = getSensorData();
+    
+    bool write_Success = false;
+    while (!write_Success) {
+        write_Success = writeToFirebase(
+            "Sensor_Data/" + String(timeinfo.tm_mon + 1) + 
+                "/" + String(timeinfo.tm_mday) + 
+                // "/" + String(timeinfo.tm_hour),
+                "/" + hour,
+            data
+        );
+    }
+    
+    hour++;
+    delay(500);
+    // while(hour != timeinfo.tm_hour + 1){
+    //     timeinfo = getDateAndTime();
+    //     delay(1000 * 60 * 60 * 10);
+    // }
 }
 
 //------------------------------
 // Function definitions
 void Wifi_Init() {
-  // Connect to WiFi
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-  Serial.print("Connecting to WiFi");
+    // Connect to WiFi
+    WiFi.disconnect(true);
+    WiFi.persistent(false);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to WiFi");
 
-  // Check connection
-  while (WiFi.status() != WL_CONNECTED) {
+    // Check connection
+    while (WiFi.status() != WL_CONNECTED) {
+        #ifdef showDetails
+        Serial.print(".");
+        #endif
+        delay(500);
+    }
+
+    // Show details
     #ifdef showDetails
-      Serial.print(".");
+        Serial.println("\nWiFi details: ");
+        Serial.print("\tIP Address: \t");
+        Serial.println(WiFi.localIP());
+        Serial.print("\tMAC Address: \t");
+        Serial.println(WiFi.macAddress());
+        Serial.print("\tSubnet Mask: \t");
+        Serial.println(WiFi.subnetMask());
+        Serial.print("\tGateway IP: \t");
+        Serial.println(WiFi.gatewayIP());
+        Serial.println();
     #endif
-    delay(500);
-  }
+}
 
-  // Show details
-  #ifdef showDetails
-    Serial.println("\nWiFi details: ");
-    Serial.print("\tIP Address: \t");
-    Serial.println(WiFi.localIP());
-    Serial.print("\tMAC Address: \t");
-    Serial.println(WiFi.macAddress());
-    Serial.print("\tSubnet Mask: \t");
-    Serial.println(WiFi.subnetMask());
-    Serial.print("\tGateway IP: \t");
-    Serial.println(WiFi.gatewayIP());
-    Serial.println();
-  #endif
+void NTP_Init() {
+    // Initialize NTP
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 }
 
 void Firebase_Init() {
-  // Initialize Firebase parameters
-  firebaseConfig.api_key = FIREBASE_API_KEY;
-  firebaseConfig.database_url = FIREBASE_URL;
-  firebaseAuth.user.email = FIREBASE_USER_EMAIL;
-  firebaseAuth.user.password = FIREBASE_USER_PASSWORD;
+    // Initialize Firebase parameters
+    firebaseConfig.api_key = FIREBASE_API_KEY;
+    firebaseConfig.database_url = FIREBASE_URL;
+    firebaseAuth.user.email = FIREBASE_USER_EMAIL;
+    firebaseAuth.user.password = FIREBASE_USER_PASSWORD;
 
-  // Firebase wifi purposes
-  Firebase.reconnectWiFi(true);
-  // firebaseData.setResponseSize(4096);
-  
-  // Firebase token generation and max amount of retries
-  firebaseConfig.token_status_callback = tokenStatusCallback;
-  // firebaseConfig.max_token_generation_retry = 5;
+    // Firebase wifi purposes
+    Firebase.reconnectWiFi(true);
+    // firebaseData.setResponseSize(4096);
+    
+    // Firebase token generation and max amount of retries
+    firebaseConfig.token_status_callback = tokenStatusCallback;
+    // firebaseConfig.max_token_generation_retry = 5;
 
-  // Initialize Firebase
-  Firebase.begin(&firebaseConfig, &firebaseAuth);
+    // Initialize Firebase
+    Firebase.begin(&firebaseConfig, &firebaseAuth);
 
-  // Print the details
-  #ifdef showDetails
-    while (firebaseAuth.token.uid == "");
-    Serial.println("Firebase details: ");
-    Serial.printf("\tUser ID: \t%s\n\n", firebaseAuth.token.uid.c_str());
-  #endif
+    // Print the details
+    #ifdef showDetails
+        while (firebaseAuth.token.uid == "");
+        Serial.println("Firebase details: ");
+        Serial.printf("\tUser ID: \t%s\n\n", firebaseAuth.token.uid.c_str());
+    #endif
 }
 
 void checkFirebaseConnection() {
-  // Check if the token has expired
-  while(Firebase.isTokenExpired()) {
-    Firebase.refreshToken(&firebaseConfig);
-  }
+    // Check if the token has expired
+    while(Firebase.isTokenExpired()) {
+        Firebase.refreshToken(&firebaseConfig);
+        #ifdef showDetails
+            Serial.println(".");
+        #endif
+    }
 }
 
-void databaseTest() {
-  if (millis() - previousMillis > interval && Firebase.ready()) {
-    previousMillis = millis();
-    
-    // Write to database
-    bool writeSuccess = Firebase.RTDB.setInt(&firebaseData, "test/int", count);
-    while (!writeSuccess) {
-      writeSuccess = Firebase.RTDB.setInt(&firebaseData, "test/int", count);
-    }
-    
-    #ifdef showDetails
-      if (writeSuccess) {
-        Serial.println("Write succesful");
-        Serial.println("\tPath: " + firebaseData.dataPath());
-        Serial.printf("\tValue: %i\n", count);
-      } else {
-        Serial.println("Write failed");
-        Serial.println("\tReason: " + firebaseData.errorReason());
-      }
-    #endif
+tm getDateAndTime(){
+    tm timeinfo;
+    while(!getLocalTime(&timeinfo));
+    return timeinfo;
+}
 
-    count++;
-  }
+sensor_data getSensorData(){
+    sensor_data data;
+    
+    // Temp values at the moment
+    data.temperature = 0;
+    data.humidity = 0;
+    data.soil_moisture = 0;
+    data.light_intensity = 0;
+    data.water_level = 0;
+
+    return data;
+}
+
+bool writeToFirebase(String path, sensor_data data){
+    for (byte i = 0; i < sensor_data_size; i++) {
+        bool writeSuccess = false;
+        
+        switch (i){
+            case 0:
+                writeSuccess = Firebase.RTDB.setFloat(&firebaseData, path + "/temperature", data.temperature);
+                break;
+            case 1:
+                writeSuccess = Firebase.RTDB.setFloat(&firebaseData, path + "/humidity", data.humidity);
+                break;
+            case 2:
+                writeSuccess = Firebase.RTDB.setFloat(&firebaseData, path + "/soil_moisure", data.soil_moisture);
+                break;
+            case 3:
+                writeSuccess = Firebase.RTDB.setFloat(&firebaseData, path + "/light_intensity", data.light_intensity);
+                break;
+            case 4:
+                writeSuccess = Firebase.RTDB.setFloat(&firebaseData, path + "/water_level", data.water_level);
+                break;
+
+            default:
+                return 0;
+        }
+
+        if (!writeSuccess) {
+            #ifdef showDetails
+                Serial.printf("Write (%hhx) failed\n", i);
+                Serial.println("\tReason: " + firebaseData.errorReason());
+            #endif
+            return 0;
+        }
+    }
+
+    return 1;
 }
 
 
